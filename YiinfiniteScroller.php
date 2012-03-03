0d6df95aa7b575da410b74200ebc2c35840baf57
apostolp <pasha.pashenko@gmail.com>
2012-03-03 12:17:26 +0200

    added two params:  - loadingimage  - JS calback function

--- YiinfiniteScroller.php
+++ YiinfiniteScroller.php
@@ -10,84 +10,115 @@
  * access all the pages.
  *
  * @author davi_alexandre
+ * 
+ * forked and improved by apostolp
  */
-class YiinfiniteScroller extends CBasePager {
+class YiinfiniteScroller extends CBasePager
+{
 
     public $contentSelector = '#content';
 
     private $_options = array(
         'loadingImg'    => null,
         'loadingText'   => null,
+        'loadingImg'    => null,
         'donetext'      => null,
         'itemSelector'  => null,
         'errorCallback' => null,
     );
 
+    private $jsCalback = '';
+
     private $_default_options = array(
-        'navSelector'   => 'div.infinite_navigation',
-        'nextSelector'  => 'div.infinite_navigation a:first',
-        'bufferPx'      => '300',
+        'navSelector' => 'div.infinite_navigation',
+        'nextSelector' => 'div.infinite_navigation a:first',
+        'bufferPx' => '50',
     );
 
-    public function init() {
-        $this->getPages()->validateCurrentPage = false;
+    public function init()
+    {
+        $this->getPages()->validateCurrentPage = FALSE;
         parent::init();
     }
 
-    public function run() {
+    public function run()
+    {
         $this->registerClientScript();
         $this->createInfiniteScrollScript();
         $this->renderNavigation();
 
-        if($this->getPages()->getPageCount() > 0 && $this->theresNoMorePages()) {
+        if ($this->getPages()->getPageCount() > 0 && $this->theresNoMorePages()) {
             throw new CHttpException(404);
         }
     }
 
-    public function __get($name) {
-        if(array_key_exists($name, $this->_options)) {
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->_options)) {
             return $this->_options[$name];
+
+        }  else if ($name == 'jsCalback') {
+            return $this->jsCalback;
         }
 
         return parent::__get($name);
     }
 
-    public function __set($name, $value) {
-        if(array_key_exists($name, $this->_options)) {
+    public function __set($name, $value)
+    {
+        if (array_key_exists($name, $this->_options)) {
             return $this->_options[$name] = $value;
+        }  else if ($name == 'jsCalback') {
+            return $this->jsCalback = $value;
         }
 
         return parent::__set($name, $value);
     }
 
-    public function registerClientScript() {
-        $url = CHtml::asset(Yii::getPathOfAlias('ext.yiinfinite-scroll.assets').'/jquery.infinitescroll.min.js');
+    public function registerClientScript()
+    {
+        $url = CHtml::asset(Yii::getPathOfAlias('ext.yiinfinite-scroll.assets') . '/jquery.infinitescroll.min.js');
         Yii::app()->clientScript->registerScriptFile($url);
     }
 
-    private function createInfiniteScrollScript() {
+    private function createInfiniteScrollScript()
+    {
         Yii::app()->clientScript->registerScript(
-                uniqid(),
-                "$('{$this->contentSelector}').infinitescroll(".$this->buildInifiniteScrollOptions().");"
+            uniqid(),
+            "$('{$this->contentSelector}').infinitescroll(" . $this->buildInifiniteScrollOptions() . ');'
         );
     }
 
-    private function buildInifiniteScrollOptions() {
+    private function buildInifiniteScrollOptions()
+    {
         $options = array_merge($this->_options, $this->_default_options);
-        $options = array_filter( $options );
+        $options = array_filter($options);
         $options = CJavaScript::encode($options);
+        $options = $this->renderJavaScriptCallback($options);
         return $options;
     }
 
-    private function renderNavigation() {
-        $next_link = CHtml::link('next',$this->createPageUrl($this->getCurrentPage(false)+1));
-        echo '<div class="infinite_navigation">'.$next_link.'</div>';
+    /**
+     * @param $options
+     * @return string
+     */
+    private function renderJavaScriptCallback($options)
+    {
+        if(!empty($this->jsCalback)) {
+            $options = $options . ',' . $this->jsCalback;
+        }
+        return $options;
     }
 
-    private function theresNoMorePages() {
+    private function renderNavigation()
+    {
+        $next_link = CHtml::link('', $this->createPageUrl($this->getCurrentPage(FALSE) + 1));
+        echo '<div class="infinite_navigation">' . $next_link . '</div>';
+    }
+
+    private function theresNoMorePages()
+    {
         return $this->getPages()->getCurrentPage() >= $this->getPages()->getPageCount();
     }
 
 }
-
-?>
